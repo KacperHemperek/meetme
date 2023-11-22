c43e89ac684aff171fdc63042e857e446059d581
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-11-22 21:12:41 +0100

    remove unused router and ignore eslint

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]): string {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   return twMerge(clsx(...inputs)) as string;
 }
