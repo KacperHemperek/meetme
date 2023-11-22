907118f4892b1d34257623b687f1a4d9e9b5006f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-11-22 21:27:52 +0100

    remove all eslint rules for cn

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -2,6 +2,6 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]): string {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  // eslint-disable-next-line
   return twMerge(clsx(...inputs)) as string;
 }
